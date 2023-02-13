b51efd84a9eb64aa6de2e790b058549cc72bfad5
silviadwiy <117156657+silviadwiy@users.noreply.github.com>
2023-02-13 07:50:20 +0700

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -24,87 +24,24 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     accuracy = correct_predictions / total_predictions
 
     output = {}
-    if phase_codename == "train":
-        print("Evaluating for Train Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    'Accuracy Score': accuracy,
-                    'Precision' : precision,
-                    'Recall' : recall,
-                    'F1 score' : f1_score,
+    
+    if phase_codename == "final":
+        print("Evaluating for Final Phase")
+        try:
+            output["result"] = [
+                {
+                    "test_split": {
+                        'Accuracy Score': accuracy,
+                        'Precision' : precision,
+                        'Recall' : recall,
+                        'F1 score' : f1_score,
+                    }
                 }
-            }
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Train Phase")
-    return output
-
-
-
-
-
-
-
-
-
-
-# import csv
-
-# def accuracy(ground_truth, predictions):
-#     correct = 0
-#     for gt, pred in zip(ground_truth, predictions):
-#         if gt[1] == pred[1]:
-#             correct += 1
-#     return correct / len(ground_truth)
-
-# def evaluate(dataset, test_annotation_file, user_submission_file, phase_codename, **kwargs):
-#     accuracy_sum = 0
-#     for item in dataset:
-#         ground_truth = item[1]
-#         item_predictions = user_submission_file[item[0]]
-#         accuracy_sum += accuracy(ground_truth, item_predictions)
-
-#     ground_truth = {}
-#     with open(test_annotation_file) as f:
-#         reader = csv.reader(f)
-#         for row in reader:
-#             if row[0] not in ground_truth:
-#                 ground_truth[row[0]] = []
-#             ground_truth[row[0]].append(row)
-    
-#     predictions = {}
-#     with open(user_submission_file) as f:
-#         reader = csv.reader(f)
-#         for row in reader:
-#             if row[0] not in predictions:
-#                 predictions[row[0]] = []
-#             predictions[row[0]].append(row)
-    
-#     dataset = list(ground_truth.items())
-#     akurasi = accuracy_sum / len(dataset)
-
-#     output = {}
-#     if phase_codename == "train":
-#         print("Evaluating for Dev Phase")
-#         output["result"] = [
-#             {
-#                 "train_split": {
-#                     'Score': akurasi,
-#                 }
-#             }
-#         ]
-#         # To display the results in the result file
-#         output["submission_result"] = output["result"][0]["train_split"]
-#         print("Completed evaluation for Dev Phase")
-#     return output
-
-        
-#     # dataset = list(ground_truth.items())
-
-
-#     # accuracy = evaluate(dataset, predictions)
-#     # print('Accuracy:', accuracy)
-
-
+            ]
+            # To display the results in the result file
+            output["submission_result"] = output["result"][0]["train_split"]
+            print("Completed evaluation for Dev Phase")
+            return output
+        except Exception as e:
+            sys.stderr.write(traceback.format_exc())
+            return e
